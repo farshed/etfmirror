7d849126b9d5efc2f1de0f3e85c6293f44fa0ee4
farshed <faisalarshed28@gmail.com>
2026-05-24 00:54:13 +0500

    Update

--- src/routes/etfs.ts
+++ src/routes/etfs.ts
@@ -2,8 +2,8 @@ import { join } from 'path';
 
 export async function listETFs() {
 	try {
-		const etfList = await Bun.file(join(process.cwd(), 'etf_list.json')).json();
-		return Response.json(etfList);
+		const file = Bun.file(join(process.cwd(), 'etf_list.json'));
+		return new Response(file, { headers: { 'Content-Type': 'application/json' } });
 	} catch (err) {
 		console.error(err);
 		return Response.json({ error: 'an error occured' }, { status: 500 });
